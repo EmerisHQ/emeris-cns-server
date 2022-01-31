75d9173a5d5448d7056f3a70482823f88bbce250
lukitsbrian <brian6.dev@gmail.com>
2022-01-31 01:42:38 -0800

    temp disable old mocks

--- mocks/AuthClient.go
+++ mocks/AuthClient.go
@@ -1,11 +1,8 @@
 package mocks
 
 import (
-	"fmt"
-	"net/http"
-	"strings"
 
-	"github.com/allinbits/emeris-cns-server/cns/middleware"
+	// "github.com/allinbits/emeris-cns-server/cns/middleware"
 	"github.com/gin-gonic/gin"
 	mock "github.com/stretchr/testify/mock"
 )
@@ -18,32 +15,32 @@ type AuthClient struct {
 // Create provides a mock function with given fields
 func (_m AuthClient) AuthUser() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		auth := middleware.AuthHeader{}
+		// auth := middleware.AuthHeader{}
 
-		if err := ctx.ShouldBindHeader(&auth); err != nil {
-			jsonErr := middleware.AuthError{
-				Error: err.Error(),
-			}
+		// if err := ctx.ShouldBindHeader(&auth); err != nil {
+		// 	jsonErr := middleware.AuthError{
+		// 		Error: err.Error(),
+		// 	}
 
-			_ = ctx.Error(err)
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, jsonErr)
-			return
-		}
+		// 	_ = ctx.Error(err)
+		// 	ctx.AbortWithStatusJSON(http.StatusUnauthorized, jsonErr)
+		// 	return
+		// }
 
-		jwtTokenHeader := strings.Split(auth.Token, " ")
+		// jwtTokenHeader := strings.Split(auth.Token, " ")
 
-		if len(jwtTokenHeader) != 2 || jwtTokenHeader[0] != "JWT" {
+		// if len(jwtTokenHeader) != 2 || jwtTokenHeader[0] != "JWT" {
 
-			err := fmt.Errorf("invalid auth token")
+		// 	err := fmt.Errorf("invalid auth token")
 
-			jsonErr := middleware.AuthError{
-				Error: err.Error(),
-			}
+		// 	jsonErr := middleware.AuthError{
+		// 		Error: err.Error(),
+		// 	}
 
-			_ = ctx.Error(err)
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, jsonErr)
-			return
-		}
+		// 	_ = ctx.Error(err)
+		// 	ctx.AbortWithStatusJSON(http.StatusUnauthorized, jsonErr)
+		// 	return
+		// }
 
 		ctx.Next()
 	}
